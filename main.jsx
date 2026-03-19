231f4c4e8647d037a7f23ee3af797812c89b35f9
BrandBoost <we.brandboost@gmail.com>
2026-03-19 16:54:59 +0500

    initial commit

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import BrandBoostStudio from './app.jsx';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrandBoostStudio />
+  </React.StrictMode>
+);
